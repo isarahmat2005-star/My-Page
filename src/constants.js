01a0cbcfa2a89a4fd8f08e76f28651048c06712b
isarahmat2005-star <isarahmat2005@gmail.com>
2026-08-31 10:39:22 +0700

    Create constants.js

---
+++ src/constants.js
@@ -0,0 +1,19 @@
+export const FONTS = [
+    { id: 'Inter', name: 'Inter' }, { id: 'Poppins', name: 'Poppins' }, { id: 'Montserrat', name: 'Montserrat' }, 
+    { id: 'Roboto', name: 'Roboto' }, { id: 'Oswald', name: 'Oswald' }, { id: 'Playfair Display', name: 'Playfair Display' },
+    { id: 'Merriweather', name: 'Merriweather' }, { id: 'Lora', name: 'Lora' }, { id: 'Space Grotesk', name: 'Space Grotesk' }, 
+    { id: 'Bebas Neue', name: 'Bebas Neue' }, { id: 'Open Sans', name: 'Open Sans' }, { id: 'Lato', name: 'Lato' }, 
+    { id: 'Nunito', name: 'Nunito' }, { id: 'Raleway', name: 'Raleway' }, { id: 'Ubuntu', name: 'Ubuntu' }, 
+    { id: 'Fira Sans', name: 'Fira Sans' }, { id: 'Quicksand', name: 'Quicksand' }, { id: 'Cinzel', name: 'Cinzel' }, 
+    { id: 'Josefin Sans', name: 'Josefin Sans' }, { id: 'Anton', name: 'Anton' },
+    { id: 'Rubik', name: 'Rubik' }, { id: 'Work Sans', name: 'Work Sans' }, { id: 'Noto Sans', name: 'Noto Sans' },
+    { id: 'PT Sans', name: 'PT Sans' }, { id: 'Karla', name: 'Karla' }, { id: 'Inconsolata', name: 'Inconsolata' },
+    { id: 'Mukta', name: 'Mukta' }, { id: 'Teko', name: 'Teko' }, { id: 'Cabin', name: 'Cabin' }, { id: 'Dosis', name: 'Dosis' },
+    { id: 'Signika', name: 'Signika' }, { id: 'Pacifico', name: 'Pacifico' }, { id: 'Zilla Slab', name: 'Zilla Slab' }, { id: 'Cairo', name: 'Cairo' },
+    { id: 'Archivo', name: 'Archivo' }, { id: 'Titillium Web', name: 'Titillium Web' }, { id: 'Varela Round', name: 'Varela Round' }, { id: 'Hind', name: 'Hind' },
+    { id: 'Abel', name: 'Abel' }, { id: 'Fjalla One', name: 'Fjalla One' }, { id: 'Dancing Script', name: 'Dancing Script' }, { id: 'Indie Flower', name: 'Indie Flower' },
+    { id: 'Caveat', name: 'Caveat' }, { id: 'Righteous', name: 'Righteous' }, { id: 'Crimson Text', name: 'Crimson Text' }, { id: 'Asap', name: 'Asap' },
+    { id: 'Exo 2', name: 'Exo 2' }, { id: 'Prompt', name: 'Prompt' }, { id: 'Manrope', name: 'Manrope' }, { id: 'Kanit', name: 'Kanit' }
+];
+
+export const ASPECT_RATIOS = ['auto', '1:1', '3:4', '4:3', '16:9', '9:16'];
