5a918f5a40c2821bdf603fcf977567678f07793c
isarahmat2005-star <isarahmat2005@gmail.com>
2026-08-31 17:05:57 +0700

    Update publish.js

--- api/publish.js
+++ api/publish.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
             contentType: 'text/html',
         });
 
-        return res.status(200).json({ url: blob.url });
+        return res.status(200).json({ url: `/api/page?id=${id}` });
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
