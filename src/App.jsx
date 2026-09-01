1aaa4515c27955bf53dfe1aaaeb9628273f7bb9b
isarahmat2005-star <isarahmat2005@gmail.com>
2026-09-01 07:09:46 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1597,6 +1597,7 @@ export default function App() {
                         </div>
                     </aside>
                 )}
+            </main>
 
             {/* PREVIEW MODAL */}
             {previewCard && (
